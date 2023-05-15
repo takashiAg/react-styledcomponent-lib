84321798be15eb2be897c59857017cea3060280a
ryosuke ando <ryo@ando.link>
2023-05-15 11:41:03 +0900

    v1.2.5

--- src/components/Spacer/Spacer.tsx
+++ src/components/Spacer/Spacer.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import classes from './Spacer.module.scss';
 import classnames from 'classnames';
-
-interface SpacerProps extends React.HTMLAttributes<HTMLSpanElement> {}
+export interface SpacerProps extends React.HTMLAttributes<HTMLSpanElement> {}
 export const Spacer: React.FC<SpacerProps> = (p) => {
   return <span {...p} className={classnames(classes.spacer, p.className)} />;
 };
